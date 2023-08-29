93f48d130ba8f5dbc6b78a8bb262c9c0b2fc7af7
Axana <kseshechka1996@gmail.com>
2023-08-29 14:28:12 +0300

    tab-58

---
+++ tab-58/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] matrix1 = {
+            { 2, 4 },
+            { 3, 2 }
+        };
+
+        int[,] matrix2 = {
+            { 3, 4 },
+            { 3, 3 }
+        };
+
+        Console.WriteLine("Первая матрица:");
+        PrintMatrix(matrix1);
+
+        Console.WriteLine("Вторая матрица:");
+        PrintMatrix(matrix2);
+
+        int[,] resultMatrix = MultiplyMatrices(matrix1, matrix2);
+
+        Console.WriteLine("Результирующая матрица:");
+        PrintMatrix(resultMatrix);
+
+        Console.ReadLine();
+    }
+
+    static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
+    {
+        int rows1 = matrix1.GetLength(0);
+        int columns1 = matrix1.GetLength(1);
+        int rows2 = matrix2.GetLength(0);
+        int columns2 = matrix2.GetLength(1);
+
+        if (columns1 != rows2)
+        {
+            throw new ArgumentException("Несовместимые размеры матриц для умножения.");
+        }
+
+        int[,] resultMatrix = new int[rows1, columns2];
+
+        for (int i = 0; i < rows1; i++)
+        {
+            for (int j = 0; j < columns2; j++)
+            {
+                int sum = 0;
+                for (int k = 0; k < columns1; k++)
+                {
+                    sum += matrix1[i, k] * matrix2[k, j];
+                }
+                resultMatrix[i, j] = sum;
+            }
+        }
+
+        return resultMatrix;
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int columns = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
